67fdea556bc2bd90cb78e796a650644705497ebb
syafa-kh <syafaatulkhayati@gmail.com>
2023-03-01 13:43:18 +0700

    Fix .h5 model

--- pred.py
+++ pred.py
@@ -3,10 +3,14 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.applications.vgg19 import preprocess_input
 
+import os
+import subprocess
+
 def pred_class(img):
     # load model
-    PATH_model = 'all_fixed-vgg19.h5'
-    model = load_model(PATH_model, compile=False)
+    if not os.path.isfile('all_fixed-vgg19.h5'):
+        subprocess.run(['curl --output model.h5 "https://github.com/syafa-kh/CV_ALLDemo/blob/main/all_fixed-vgg19.h5"'], shell=True)
+    model = load_model('model.h5', compile=False)
 
     # prepare image
     img_prep = np.expand_dims(img_to_array(img), axis=0)
