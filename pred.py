2caaafb2bf05d1236b5fa5d02031bca11669fcb5
syafa-kh <syafaatulkhayati@gmail.com>
2023-03-01 15:02:16 +0700

    still fixing load_model

--- pred.py
+++ pred.py
@@ -4,17 +4,18 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.applications.vgg19 import preprocess_input
 
-import os
-import urllib.request
-@st.cache_resource
-def load_model():
-    if not os.path.isfile('model.h5'):
-        urllib.request.urlretrieve('https://github.com/syafa-kh/CV_ALLDetection/raw/main/all_fixed-vgg19.h5', 'model.h5')
-    return tf.keras.models.load_model('model.h5')
+# import os
+# import urllib.request
+# @st.experimental_singleton
+# def load_model():
+#     if not os.path.isfile('model.h5'):
+#         urllib.request.urlretrieve('https://github.com/syafa-kh/CV_ALLDetection/raw/main/all_fixed-vgg19.h5', 'model.h5')
+#     return tf.keras.models.load_model('model.h5')
 
 def pred_class(img):
     # load model
-    model = load_model()
+    PATH_model = 'all_fixed-vgg19.h5'
+    model = tf.keras.models.load_model(PATH_model, compile=False)
 
     # prepare image
     img_prep = np.expand_dims(img_to_array(img), axis=0)
